a5ec6a1b13332b3623e99f2166b138ff1437a5ca
Rurumi <sala030782@gmail.com>
2021-06-28 22:59:46 +0800

    1

--- app.py
+++ app.py
@@ -1,37 +1,54 @@
-# 初始化LINT BOT
-import os
+from flask import Flask, request, abort
 
-from flask import Flask
-from linebot import LineBotApi, WebhookHandler
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage,ImageMessage)
+import json
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['sBIB8FLCO7qzl4tQCbbT1MQChyb/uiYCTxySE4rLqholm+kTjgsxXUfFCQkvl/k95dMJLUDo87Afy7u8dS4biwLzezr0Bb6bu2PUdjKie7+aTxmgX5QHUplrn+oHAdOTUNMdspQXJ9AuzA2lRjMfAgdB04t89/1O/w1cDnyilFU='])
-handler = WebhookHandler(os.environ['b9f7acc632cb65d50b8af479e73cfc87'])
+secretFile = json.load(open("secretFile.txt",'r'))
+channelAccessToken = secretFile['channelAccessToken']
+channelSecret = secretFile['channelSecret']
 
-# 利用 handler 處理 LINE 觸發事件
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+line_bot_api = LineBotApi(channelAccessToken)
+handler = WebhookHandler(channelSecret)
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
-    )
-
-# 利用 route 處理路由
-from flask import request, abort
-from linebot.exceptions import InvalidSignatureError
-
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
+
+@handler.add(MessageEvent, message=(ImageMessage))
+def handle_image_message(event):
+    # 使用者傳送的照片
+    message_content = line_bot_api.get_message_content(event.message.id)
+
+    # 照片儲存名稱
+    fileName = event.message.id + '.jpg'
+
+    # 儲存照片
+    with open('./image/' + fileName, 'wb')as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+
+    # linebot回傳訊息
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='收到您上傳的照片囉!'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
