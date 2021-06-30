94f32d53eb94915b2e81f31f4864fddc3d89618c
Rurumi <sala030782@gmail.com>
2021-06-30 15:30:38 +0800

    xxx2

--- app.py
+++ app.py
@@ -39,20 +39,39 @@ def callback():
         abort(400)
 
     return 'OK'
-    
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+
+@handler.add(MessageEvent, message=(ImageMessage))
+def handle_image_message(event):
+    # 使用者傳送的照片
     message_content = line_bot_api.get_message_content(event.message.id)
-    data = selectdb.Mongo_select(int(message_content))
-    ans = receipt(list('12345678'),data)
 
+    # 照片儲存名稱
+    fileName = event.message.id + '.jpg'
+
+    # 儲存照片
+    with open('./image/' + fileName, 'wb')as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+
+    # linebot回傳訊息
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=ans))
+        TextSendMessage(text='收到您上傳的照片囉!'))
 
 
-    print(event.message.text) # 接收用戶訊息
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message_content = line_bot_api.get_message_content(event.message.id)
+#     data = selectdb.Mongo_select(int(message_content))
+#     ans = receipt(list('12345678'),data)
+
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=ans))
+
+
+#     print(event.message.text) # 接收用戶訊息
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
