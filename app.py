7c8f15bef334e4ce15b02c52da4778de8f54d5b8
Rurumi <sala030782@gmail.com>
2021-07-02 00:39:50 +0800

    xxend

--- app.py
+++ app.py
@@ -7,20 +7,18 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import json
 from model import formrecognizer_by_url,receipt,selectdb
-from datetime import datetime
-import pyimgur
+import tempfile, os
+from imgurpython import ImgurClient
+from config import *
 
 app = Flask(__name__)
 
 # LINE BOT info
-secretFile = json.load(open("secretFile.txt",'r'))
-channelAccessToken = secretFile['channelAccessToken']
-channelSecret = secretFile['channelSecret']
-
 line_bot_api = LineBotApi(channelAccessToken)
 handler = WebhookHandler(channelSecret)
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'image', 'tmp')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -43,48 +41,52 @@ def handle_message(event):
     message_type = event.message.type
     user_id = event.source.user_id
 
-    if message_type=='image':
-        message_id = event.message.id
-        message_content = line_bot_api.get_message_content(message_id)
-        fileName = message_id + '.jpg'
-        with open('./image/' + fileName, 'wb')as f:
+    if isinstance(event.message, ImageMessage):
+
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix='jpg' + '-', delete=False) as tf:
             for chunk in message_content.iter_content():
-                f.write(chunk)
+                tf.write(chunk)
+            tempfile_path = tf.name
         #上傳圖片   
+        
+        dist_path = tempfile_path + '.jpg'
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+        try:
+            client = ImgurClient(Client_ID, Client_secret, access_token, refresh_token)
+            config = {
+                'album': album_id,
+                'name': 'Catastrophe!',
+                'title': 'Catastrophe!',
+                'description': 'invoice'
+            }
+            path = os.path.join('image', 'tmp', dist_name)
+            images = client.upload_from_path(path, config=config, anon=False)
+            print(images)
+            image_url = images['link']
 
-        CLIENT_ID = secretFile['Client_ID']
-        PATH = './image/' + fileName #A Filepath to an image on your computer"
-
-        im = pyimgur.Imgur(CLIENT_ID)
-        uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")
-        image_url = uploaded_image.link
-        number_id = formrecognizer_by_url.formrecognizer_by_url(image_url) 
-        invoice_date = number_id['日期']       
-        invoice_number = number_id['發票號碼']  
-        receipt_data = selectdb.Mongo_select(int(invoice_date))
-        ans = receipt.receipt_mechine([invoice_number],receipt_data)
-
-        # 回傳訊息的方法
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ans[0][2]))
-    elif message_type== 'text':
+            number_id = formrecognizer_by_url.formrecognizer_by_url(image_url) 
+            invoice_date = number_id['日期']       
+            invoice_number = number_id['發票號碼']  
+            receipt_data = selectdb.Mongo_select(int(invoice_date))
+            ans = receipt.receipt_mechine([invoice_number],receipt_data)
+            # 回傳訊息的方法
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ans[0][2]))   
+            return 0
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳失敗'))
+            return 0
+        
+    elif isinstance(event.message, TextMessage):
         message = event.message.text
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = message))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "error"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Error"))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     message_content = line_bot_api.get_message_content(event.message.id)
-#     data = selectdb.Mongo_select(int(message_content))
-#     ans = receipt(list('12345678'),data)
-
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=ans))
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
